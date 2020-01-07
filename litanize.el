d1d2a8c646e030f163dc5fb49dd906fb3269b983
nik gaffney <nik@fo.am>
2020-01-08 09:21:02 +1030

    Otavalo Canton, Gilet, Madejski, The Cullman Times & Acacia sulcata.

--- litanize.el
+++ litanize.el
@@ -32,19 +32,14 @@
 ;; from random wikipedia titles as an exercise in ontography, metaphorism,
 ;; and carpentry
 ;;
-;; Currently uses random wikipedia titles as elements.  Might be extended to
-;; use other lists in other futures.
+;; Currently uses wikipedia titles as elements. Might be extended to use
+;; other lists in other futures.
 ;; 
-;; 'M-x litanize'  will generate a litany in a new buffer
+;; 'M-x litanize' will generate a litany in a new buffer
 ;;
-;; 'M-x insert-litany' will generate a litany at the point
+;; 'M-x litanize-at-point' will generate a litany at the point
 ;;
 ;; further -> http://bogost.com/writing/blog/latour_litanizer/
-
-;;; Revision history:
-;; 
-;;  - 2019-01-19 - preliminary carpentry
-;;  - 2019-12-12 - melpa emersion, stochastism
 
 
 ;;; Code:
